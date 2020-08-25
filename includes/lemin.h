a268a4558bc92933559e1d009aa343873fa6abd0
Anton Gorobets <a.p.gorobets@gmail.com>
2020-08-25 15:26:44 +0300

    ft_parse_ants()

--- includes/lemin.h
+++ includes/lemin.h
@@ -24,55 +24,56 @@
 **--------------------------------defines---------------------------------------
 */
 
-# define HELP		"use flag -v for visualizer\n"
-# define IS_BUSY	1
-# define IS_FREE	0
+# define HELP			"use flag -v for visualizer\n"
+# define ANTS_NOT_VALID	"Please, enter the number of ants correctly.\n"
+# define IS_BUSY		1
+# define IS_FREE		0
 
-# define ROOM		"t_room"
-# define LEMIN		"t_lemin"
-# define DATA		"t_data"
-# define EASY_FREE	NULL
+# define ROOM			"t_room"
+# define LEMIN			"t_lemin"
+# define DATA			"t_data"
+# define EASY_FREE		NULL
 
 /*
 **--------------------------------structs---------------------------------------
 */
 
-typedef struct		s_data
+typedef struct			s_data
 {
-	char			*content;
-	struct s_data	*next;
-}					t_data;
+	char				*content;
+	struct s_data		*next;
+}						t_data;
 
-typedef struct		s_room
+typedef struct			s_room
 {
-	int				x;
-	int				y;
-	int				state;
-	int				weight;
-	int				*links;
-}					t_room;
+	int					x;
+	int					y;
+	int					state;
+	int					weight;
+	int					*links;
+}						t_room;
 
-typedef struct		s_lemin
+typedef struct			s_lemin
 {
-	int				visual;
-	int				ants;
-	int				start;
-	int				end;
-	t_room			*graph;
-}					t_lemin;
+	int					visual;
+	int					ants;
+	int					start;
+	int					end;
+	t_room				*graph;
+}						t_lemin;
 
 /*
 **-------------------------------functions--------------------------------------
 */
 
-t_lemin				*ft_lemin_init(void);
+t_lemin					*ft_lemin_init(void);
 
-void				ft_error_n_exit(char *str, void *mem, char *type);
-void				ft_free_data(t_data *data);
+void					ft_error_n_exit(char *str, void *mem, char *type);
+void					ft_free_data(t_data *data);
 
-void				ft_check_flags(t_lemin *lemin, int argc, char *argv[]);
-t_lemin				*ft_parse_data(t_lemin *lemin);
-int					ft_get_data(t_data **data);
-void				ft_parse_ants(t_lemin *lemin, t_data **data);
+void					ft_check_flags(t_lemin *lemin, int argc, char *argv[]);
+t_lemin					*ft_parse_data(t_lemin *lemin);
+int						ft_get_data(t_data **data);
+void					ft_parse_ants(t_lemin *lemin, t_data **data);
 
 #endif
--- sources/ft_parse_ants.c
+++ sources/ft_parse_ants.c
@@ -17,6 +17,12 @@ void		ft_parse_ants(t_lemin *lemin, t_data **data)
 	if (ft_get_data(data))
 	{
 		ft_free_data(*data);
-		ft_error_n_exit("Error in ft_parse_ants()", lemin, LEMIN);
+		ft_error_n_exit("Error in ft_parse_ants()\n", lemin, LEMIN);
+	}
+	if (!ft_str_is_int((*data)->content) \
+		|| (lemin->ants = ft_atoi((*data)->content)) < 1)
+	{
+		ft_free_data(*data);
+		ft_error_n_exit(ANTS_NOT_VALID, lemin, LEMIN);
 	}
 }
