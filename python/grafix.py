612693006943211289e9a3d843ef0e74a4210b73
ksemele <greengrunt@gmail.com>
2020-09-08 14:52:47 +0300

    misc updates, add "support" module

--- python/grafix.py
+++ python/grafix.py
@@ -1,4 +1,6 @@
+import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -6,52 +8,60 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationTool
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import matplotlib.patches as mpatches
-
-
-import tkinter
+import support as s
 import numpy as np
+import buttons as btn
 
 
-def about():
+def ft_about():
     a = Toplevel()
     a.geometry('500x100')
     # a['bg'] = 'gray'
     a.overrideredirect(True)
-    Label(a, text="This is a visualiser for lem in by a cghael && ksemele!").pack(expand=1)
+    Label(a, text="This is a visualiser for lem in by a cghael && ksemele!").grid()
     a.after(1000, lambda: a.destroy())  # autokill window after 1000 ms
 
 
-def init_window(g):
+def ft_open_map():
+    # destroy figure
+    # open new .map
+    # parse, draw
+    s.ft_print_func_name("open map")
+
+
+def ft_embed_graph(g, root):
+    pos = nx.spring_layout(g)
+    nx.draw(g, pos, node_color='', edge_color='blue', with_labels=True)
+    # red patch with Ants marker?
+    red_patch = mpatches.Patch(color='red', label='Ants')
+    plt.legend(handles=[red_patch])
+    canvas = FigureCanvasTkAgg(plt.figure(1), master=root)
+    canvas.draw()
+    canvas.get_tk_widget().grid()
+
+
+def ft_destroy_graph():
+    s.ft_print_func_name("destroy")
+
+
+def ft_init_window(g):
     root = Tk()
+    root.tk_setPalette('gray60')
     width = 600
     height = 600
     w = root.winfo_screenwidth()  # width of all screen
     h = root.winfo_screenheight()  # height of all screen
-    # w = w // 2  # for center on screen
+    w = w // 2  # for center on screen by a X
     h = h // 2
     w = w - width // 2
     h = h - height // 2
     root.geometry('600x600+{}+{}'.format(w, h))  # create window with shift
     root.title("lemin visualiser v 0.1")
-
-    Button(text="Open map", width=20).pack()
-    # Label(text="Имя карты?", width=20, height=3).pack()
-    Button(text="About", width=20, command=about).pack()
-
-
-    print("len g is: ", len(g))
-# graph plot
-#     nx.draw(g, with_labels=True)
-
-    def embed_graph(g):
-        pos = nx.spring_layout(g)
-        nx.draw(g, pos, node_color='', edge_color='blue', with_labels=True)
-        # red patch with Ants marker?
-        red_patch = mpatches.Patch(color='red', label='Ants')
-        plt.legend(handles=[red_patch])
-        canvas = FigureCanvasTkAgg(plt.figure(1), master=root)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side="top")
-
-    embed_graph(g)
+# buttons
+    Button(text="Open map", width=10, command=ft_open_map).grid()
+    Button(text="About", width=10, command=ft_about).grid()
+    Button(text="destroy", width=10, command=ft_destroy_graph).grid()
+# end buttons
+    print("len g is: ", len(g))  #todo
+    ft_embed_graph(g, root)  # including graph ^^^
     root.mainloop()
--- python/main.py
+++ python/main.py
@@ -1,124 +1,26 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import sys
 from termcolor import colored, cprint  # цветной cprint https://pypi.org/project/termcolor/
 # from collections import deque # двусторонняя очередь
-# import Tkinter as tk
-# from random import *
 from tkinter import *
 import networkx as nx
 import matplotlib.pyplot as plt
 from sys import argv
-import grafix
-
-
-# from pyparsing import *
-# import re
-
-
-def print_func_name(name):
-    # end=" " - аттрибут, который меняет "\n" по-умолчанию на " ", чтобы принтилось в одну строку.
-    cprint("func:", 'cyan', end=" ")
-    cprint("{}\n".format(name), 'green')
-
-
-# def print_graph(graph):
-    # print_func_name('print graph')
-    # for row in graph:
-    #     for item in row:
-    #         cprint("row [{}]:".format(item), end=" ")
-    #         cprint("->", 'grey', end=" ")
-    #         cprint(row[item], 'white', end=" ")
-    #         cprint("")
-
-
-def init_graph(g, argv):
-    print_func_name('init graph')
-    # open file
-    file = open("../test", 'r')
-    # print('im readed file: ', file.read())  # open all data
-    check_start = 0
-    check_end = 0
-    for line in file:
-        # cprint('curr line is: '+line, color='green')
-        if line == '##start\n':
-            check_start = 1
-        elif line == '##end\n':
-            check_end = 1
-    if check_start == 1 and check_end == 1:
-        cprint('start && end founded!', color='yellow')
-
-# START parsing mb need create class?
-    cprint("open file: "+argv[1])  # print argv[1]
-    file = open(argv[1])
-    ants = int(file.readline())
-    print("ANTS from argv:", ants)
-    rooms = []
-    check_start = 0
-    check_end = 0
-    for line in file:
-        # check & save start, end
-        if check_start == 1:
-            # rooms.append("start->")
-            start_name = line.split().pop(0)
-            print("start name: ", start_name)
-            check_start = 0
-        if check_end == 1:
-            # rooms.append("end->")
-            end_name = line.split().pop(0)
-            print("end name: ", end_name)
-            check_end = 0
-        if line == '##start\n':
-            check_start = 1
-        elif line == '##end\n':
-            check_end = 1
-        if ' ' not in line and line[0] != '#':
-            break
-        if line[0] != '#':
-            rooms.append(line.split())
-    print(rooms)
-    print(len(rooms))
-    file = open(argv[1])
-    edges = []
-    for line in file:
-        if line[0] != '#' and '-' in line:
-            line = ' '.join(line.split())  # remove '\n' for correctly splitting
-            edges.append(line.split(sep='-'))
-    print(edges)
-    print(len(edges))
-    # END parsing
-    print("len g is: ", len(g))
-    g.clear()
-    print("len g is: ", len(g))
-
-    # fill Graph from list
-    # fill all node_names
-    r_names = rooms.copy()
-    for i in range(len(r_names)):
-        g.add_node(r_names.pop().pop(0))
-    print("nodes in g: ", g.nodes)
-    # fill all edges
-    g.add_edges_from(edges)
-    # end fill Graph
-
-    print("len g is: ", len(g), "len rooms: ", len(rooms))
-
-    return [g]
+import grafix as gx
+import support as s
 
 
 if __name__ == '__main__':
     # init graph
     G = nx.Graph()
-    init_graph(G, argv)
+    s.ft_init_graph(G, argv)
     # print_graph(G)
     # G.Graph('Olt2') {color='red'}
 
-# ############################################# GRAFIX
-    grafix.init_window(G)
+    # ############################### GRAFIX ##############################
+    gx.ft_init_window(G)
 
-# ############################################# GRAFIX
+    # ############################### GRAFIX ##############################
     cprint("\nEND\n", 'magenta')
+
+
+# ############################### TEST ##############################
